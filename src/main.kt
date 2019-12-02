f7acfe236a75e382bd7f14e588846b41c7141634
celticg <celtic.g@gmail.com>
2019-12-02 14:38:18 -0500

    Time calculator works
    Now working on Slippage calaculator

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,6 @@
 import java.awt.EventQueue
 import javax.swing.JButton
 import javax.swing.JFrame
-import javax.swing.JTextArea
 import javax.swing.JTextField
 
 class mainUI(title: String) : JFrame(){
@@ -20,7 +19,6 @@ class mainUI(title: String) : JFrame(){
         pane.layout = null
 
         val btnCalc = JButton("Calculate")
-        val btnGoToSlip = JButton("Slip?")
         val calculation = JTextField(25)
         val answer = JTextField(10)
         btnCalc.addActionListener {
@@ -35,27 +33,14 @@ class mainUI(title: String) : JFrame(){
             AddTime.addingTime1()
             answer.text = AddTime.answer
         }
-        btnGoToSlip.addActionListener {
-            EventQueue.invokeLater(::openSlips)
-        }
-
         calculation.setBounds(16,16,200,40)
         btnCalc.setBounds(232,16,80,40)
         answer.setBounds(16, 70, 80, 40)
-        btnGoToSlip.setBounds(232, 72, 80, 42)
 
         pane.add(btnCalc)
-        pane.add(btnGoToSlip)
         pane.add(calculation)
         pane.add(answer)
     }
-}
-private fun openSlips(){
-
-    val slips = slippageFinderUI("Slippage")
-    slips.isVisible = true
-
-    //val testing = test("Hi?")
 }
 
 private fun createAndShowGUI(){
